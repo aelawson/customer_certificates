cf378b2a1e7963a86b0999d61c1002332d4077cf
Andrew Lawson <alawson@jwplayer.com>
2018-07-28 14:53:52 -0400

    Add Config service and config.yaml file for env setup. Add pyYAML package. Update docker-compose to use env var for local setup.

---
+++ src/services/config.py
@@ -0,0 +1,12 @@
+import yaml
+import os
+
+env = os.environ.get('env', 'prd')
+
+with open('config.yaml') as conf:
+    conf_base = yaml.safe_load(conf)
+
+try:
+    Config = conf_base[env]
+except KeyError:
+    raise Exception('Invalid environment! Make sure your $env var is set properly.')
