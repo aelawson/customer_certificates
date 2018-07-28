8ef8d839bc1d9e3ad9391caf85bd1b3e6915096c
Andrew Lawson <alawson@jwplayer.com>
2018-07-28 17:49:01 -0400

    Add content-type enforcement middleware. Add middleware to app / set error serializer.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import falcon
 
+from src.middleware.error_serializer import ErrorSerializer
+from src.middleware.content_type import ContentTypeMiddleware
 from src.middleware.session import RequestSessionMiddleware
 from src.services.db import DbSession
 
@@ -20,8 +22,10 @@ def create():
     Creates app instance and instantiates middleware / routes.
     """
     app = falcon.API(middleware=[
+        ContentTypeMiddleware(),
         RequestSessionMiddleware(DbSession)
     ])
+    app.set_error_serializer(ErrorSerializer)
 
     app.add_route('/hello', HelloResource())
 
---
+++ src/middleware/content_type.py
@@ -0,0 +1,18 @@
+import falcon
+
+class ContentTypeMiddleware:
+    """
+    Middleware for restricting / enforcing request and response formats.
+    """
+
+    def process_request(self, req, resp):
+        if not req.client_accepts_json:
+            raise falcon.HTTPNotAcceptable(
+                description='JSON is the only supported response format'
+            )
+
+        if req.method in ('POST', 'PUT'):
+            if 'applicaton/json' not in req.content_type:
+                raise falcon.HTTPUnsupportedMediaType(
+                    description='JSON is the only supported request format'
+                )
