2f90388f63cc55dc9fe7224b11fbc87cfc924936
Andrew Lawson <alawson@jwplayer.com>
2018-07-28 21:50:40 -0400

    Fix typo.

--- src/middleware/content_type.py
+++ src/middleware/content_type.py
@@ -12,7 +12,7 @@ class ContentTypeMiddleware:
             )
 
         if req.method in ('POST', 'PUT'):
-            if req.content_type and 'applicaton/json' not in req.content_type:
+            if req.content_type and 'application/json' not in req.content_type:
                 raise falcon.HTTPUnsupportedMediaType(
                     description='JSON is the only supported request format'
                 )
