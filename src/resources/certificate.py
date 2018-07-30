9ef889a9b0a95c71e85a0841411f329e4dbaf821
Andrew Lawson <alawson@jwplayer.com>
2018-07-29 23:59:53 -0400

    Add certificate active resource.

--- src/resources/certificate.py
+++ src/resources/certificate.py
@@ -2,18 +2,21 @@ import base64
 import falcon
 import json
 
+from requests.exceptions import HTTPError
 from sqlalchemy.orm.exc import NoResultFound
 
 from src.models.certificate import Certificate
+from src.services.certificate import CertificateService
 
 class CertificatesResource:
     """
     Resource for creating and retrieving certificates
+    Endpoint: /users/{user_id}/certificates
     """
 
     def on_post(self, req, resp, **kwargs):
         """
-        Handles POST requests to the /users/{user_id}/certificates endpoint
+        Handles POST requests.
         Creates a certificate resource for a user.
         """
         try:
@@ -39,7 +42,7 @@ class CertificatesResource:
 
     def on_get(self, req, resp, **kwargs):
         """
-        Handles GET requests to the /users/{user_id}/certificates endpoint
+        Handles GET requests.
         Lists certificate resources for a user.
         """
         try:
@@ -54,3 +57,54 @@ class CertificatesResource:
         resp.body = json.dumps(list(
             map(lambda c: { 'id': c.id, 'user_id': c.user_id, 'body': c.body }, certs)
         ))
+
+class CertificateActiveResource():
+    """
+    Resource for activating/deactivating a certificate for a user.
+    Endpoint: /users/{user_id}/certificates/{certificate_id}/active
+    """
+
+    def on_patch(self, req, resp, **kwargs):
+        """
+        Handles PATCH requests.
+        Updates active state for a certificate resource.
+        """
+        payload = json.loads(req.stream.read().decode('utf-8'))
+
+        if len(payload) != 1 or 'active' not in payload:
+            raise falcon.HTTPBadRequest(
+                description='Must provide only the following fields: active'
+            )
+
+        # Update active status in the DB
+        try:
+            cert = self.session.query(Certificate)\
+                .filter(Certificate.id == kwargs.get('certificate_id'))\
+                .filter(Certificate.user_id == kwargs.get('user_id')).one()
+
+            print(kwargs.get('certificate_id'))
+            print(cert)
+            if payload['active'] == cert.active:
+                raise falcon.HTTPBadRequest(
+                    description='Certificate is already in the requested active state'
+                )
+
+            cert.active = payload['active']
+        except NoResultFound:
+            raise falcon.HTTPNotFound(
+                description='No certificate found for the specified user and certificate'
+            )
+
+        # Notify external service of change
+        try:
+            CertificateService.notify(cert.id, cert.active)
+        except HTTPError as e:
+            raise falcon.HTTPServiceUnavailable(
+                description="Internal service error: {error}".format(error=e)
+            )
+
+        resp.status = falcon.HTTP_202
+        resp.body = json.dumps({
+            'id': cert.id,
+            'active': cert.active
+        })
