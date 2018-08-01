6a32759e4855f814b835cb2a1367d453ec61688e
Andrew Lawson <alawson@jwplayer.com>
2018-07-31 23:32:44 -0400

    Add composite index on user_id and id.

--- src/models/certificate.py
+++ src/models/certificate.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, Boolean, LargeBinary, Text
-from sqlalchemy.schema import ForeignKey
+from sqlalchemy.schema import ForeignKey, Index
 
 from src.models import Base
 from src.models.user import User
@@ -12,6 +12,8 @@ class Certificate(Base):
     private_key = Column(LargeBinary, nullable=False)
     active = Column(Boolean, nullable=False)
     body = Column(Text, nullable=False)
+
+    __table_args__ = (Index('user_id_and_id_index', "user_id", "id"), )
 
     def __repr__(self):
         return "Certificate<(id={id}, user_id={user_id}, active={active})>".format(
